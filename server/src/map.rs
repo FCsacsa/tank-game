69ec91f6585e393e337d46e61c6acb28612f7871
Csanád Farkas <farkas.csacsa@gmail.com>
2025-06-21 16:13:50 +0200

    change map format handling

--- server/src/map.rs
+++ server/src/map.rs
@@ -1,9 +1,16 @@
 use bevy::{
-    asset::AssetServer, ecs::{component::Component, resource::Resource, system::{Commands, Res}}, math::{Dir2, Vec2}, sprite::Sprite, transform::components::Transform
+    asset::AssetServer, color::Color, ecs::{
+        component::Component,
+        resource::Resource,
+        system::{Commands, Res},
+    }, log, math::{Dir2, Vec2}, sprite::Sprite, transform::components::Transform
 };
 use serde::Deserialize;
 
-use crate::{entities::{self, Spawn}, util::{with_z, with_z_arr}};
+use crate::{
+    entities::{self, Spawn},
+    util::{with_z, with_z_arr},
+};
 
 #[derive(Deserialize)]
 struct Wall {
@@ -14,6 +21,7 @@ struct Wall {
 
 #[derive(Deserialize)]
 pub struct Map {
+    name: String,
     background_path: String,
     walls: Vec<Wall>,
     spawns: Vec<[f32; 2]>,
@@ -28,25 +36,28 @@ impl Map {
             ))
             .with_children(|parent| {
                 for wall in &self.walls {
-                    let from : Vec2 = wall.from.into();
-                    let to : Vec2 = wall.to.into();
+                    let from: Vec2 = wall.from.into();
+                    let to: Vec2 = wall.to.into();
                     let position = with_z((from + to) / 2.0, 0.0);
-
-                    parent.spawn((
-                        entities::Wall {
-                            normal: Dir2::from_xy_unchecked(wall.normal[0], wall.normal[1]),
-                            direction: Dir2::from_xy_unchecked(from.x - to.x, from.y - to.y),
-                            half_length: (to - from).length() / 2.0,
-                        },
-                        Transform::from_translation(position),
-                    ));
+                    if let Ok((direction, length)) = Dir2::new_and_length(to - from) {
+                        let normal = Dir2::from_xy(wall.normal[0], wall.normal[1]).unwrap_or(
+                            Dir2::new_unchecked(direction.rotate(Vec2 { x: 0.0, y: 1.0 })),
+                        );
+                        parent.spawn((
+                            entities::Wall {
+                                normal,
+                                direction,
+                                half_length: length / 2.0,
+                            },
+                            Transform::from_translation(position),
+                        ));
+                    } else {
+                        log::warn!("The current map \"{}\" contains 0 length walls.", self.name);
+                    }
                 }
                 for &spawn in &self.spawns {
                     let pos = with_z_arr(spawn, 0.0);
-                    parent.spawn((
-                        Spawn(),
-                        Transform::from_translation(pos),
-                    ));
+                    parent.spawn((Spawn(), Transform::from_translation(pos)));
                 }
             });
     }
