e8c1dac95c71c245abf0c37ccccf3c6c9b6fd4ae
Csanád Farkas <farkas.csacsa@gmail.com>
2025-06-25 14:21:49 +0200

    make client not require restarts

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,5 @@
-use std::env::var;
 use std::net::UdpSocket;
+use std::{env::var, time::Duration};
 
 use messages::{client::ClientMessages, server::ServerMessages};
 
@@ -14,59 +14,64 @@ fn main() {
         .unwrap_or(4000);
     let socket = UdpSocket::bind(("127.0.0.1", self_port)).expect("could not bind port");
 
-    let msg = ClientMessages::Connect { self_port };
-    let msg_vec = Vec::from(&msg);
+    socket.set_read_timeout(Some(Duration::new(5, 0))).expect("whoops");
 
-    socket
-        .send_to(&msg_vec, ("127.0.0.1", server_port))
-        .expect("could not send");
+    loop {
+        let msg = ClientMessages::Connect { self_port };
+        let msg_vec = Vec::from(&msg);
 
-    let mut buf = [0; 4885]; // at minimum 4883
+        socket
+            .send_to(&msg_vec, ("127.0.0.1", server_port))
+            .expect("could not send");
 
-    while let Ok(msg) = socket.recv(&mut buf) {
-        let parsed =
-            ServerMessages::try_from(&buf[..]).expect("should not have ill formatted messages");
-        println!("{parsed:?}");
-        match parsed {
-            ServerMessages::MapChange { secret, walls } => {
-                // let msg = ClientMessages::Control {
-                //     self_port,
-                //     secret,
-                //     tracks_acceleration_target: walls[0].origin,
-                //     turret_acceleration_target: walls[0].direction_length[1],
-                //     shoot: secret % 2 == 1,
-                // };
-                // let msg_vec = Vec::from(&msg);
-                // socket
-                //     .send_to(&msg_vec, ("127.0.0.1", server_port))
-                //     .unwrap();
-                // println!("sent: {msg:?}")
-            }
-            ServerMessages::State {
-                secret,
-                tanks,
-                bullets,
-            } => {
-                socket
-                    .send_to(
-                        &Vec::from(&ClientMessages::Control {
-                            self_port,
-                            secret,
-                            tracks_acceleration_target: [-10.0, 10.0],
-                            turret_acceleration_target: 50.0,
-                            shoot: true,
-                        }),
-                        ("127.0.0.1", server_port),
-                    )
-                    .unwrap();
-            }
-            ServerMessages::Disconnected => {
-                socket
-                    .send_to(
-                        &Vec::from(&ClientMessages::Connect { self_port }),
-                        ("127.0.0.1", server_port),
-                    )
-                    .unwrap();
+        let mut buf = [0; 4885]; // at minimum 4883
+
+        while let Ok(_) = socket.recv(&mut buf) {
+            let parsed =
+                ServerMessages::try_from(&buf[..]).expect("should not have ill formatted messages");
+            println!("{parsed:?}");
+            match parsed {
+                ServerMessages::MapChange { secret, walls } => {
+                    // let msg = ClientMessages::Control {
+                    //     self_port,
+                    //     secret,
+                    //     tracks_acceleration_target: walls[0].origin,
+                    //     turret_acceleration_target: walls[0].direction_length[1],
+                    //     shoot: secret % 2 == 1,
+                    // };
+                    // let msg_vec = Vec::from(&msg);
+                    // socket
+                    //     .send_to(&msg_vec, ("127.0.0.1", server_port))
+                    //     .unwrap();
+                    // println!("sent: {msg:?}")
+                }
+                ServerMessages::State {
+                    secret,
+                    tanks,
+                    bullets,
+                } => {
+                    socket
+                        .send_to(
+                            &Vec::from(&ClientMessages::Control {
+                                self_port,
+                                secret,
+                                // tracks_acceleration_target: [1000.0, 1000.0],
+                                tracks_acceleration_target: [0.0, 0.0],
+                                turret_acceleration_target: 0.0,
+                                shoot: true,
+                            }),
+                            ("127.0.0.1", server_port),
+                        )
+                        .unwrap();
+                }
+                ServerMessages::Disconnected => {
+                    socket
+                        .send_to(
+                            &Vec::from(&ClientMessages::Connect { self_port }),
+                            ("127.0.0.1", server_port),
+                        )
+                        .unwrap();
+                }
             }
         }
     }
