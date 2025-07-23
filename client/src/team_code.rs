c579d490fbccbecb0d1d9533de65a1982977d5c6
Csanád Farkas <farkas.csacsa@gmail.com>
2025-07-23 15:50:36 +0200

    add comments to client

--- client/src/team_code.rs
+++ client/src/team_code.rs
@@ -1,3 +1,5 @@
+// THIS IS WHERE YOU SHOULD PUT YOUR CODE.
+
 use messages::{client::ClientMessages, server::ServerMessages};
 
 #[derive(Default)]
@@ -6,17 +8,30 @@ pub struct State {
 }
 
 impl State {
+    // THIS IS THE FUNCTION WHERE YOU CAN CONTROL YOUR TANK
     pub fn handle_message(&mut self, incoming: &ServerMessages) -> Option<ClientMessages> {
         match incoming {
             ServerMessages::MapChange {
                 secret: _,
                 walls: _,
-            } => None,
+            } => {
+                // HANDLE MAP CHANGE -- clear current state?
+                // No need for input, return None
+                None
+            }
             ServerMessages::State {
                 secret,
                 tanks: _,
                 bullets: _,
-            } => Some(ClientMessages::control([0.0, 0.0], 0.0, true, *secret)),
+            } => {
+                // HANDLE GENERAL STATE CHANGE
+                // put your logic here, potentially update your state
+
+                // return your desired controls:
+                Some(ClientMessages::control([0.0, 0.0], 0.0, true, *secret))
+            }
+            // In case we got dropped, try to reconnect.
+            // No need to change this.
             ServerMessages::Disconnected => Some(ClientMessages::connect()),
         }
     }
