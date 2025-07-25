b730d9e2ac96d5ea3beb37c93e739f78ebc8ef80
Csanád Farkas <farkas.csacsa@gmail.com>
2025-07-25 16:58:16 +0200

    remake client

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,6 +3,7 @@ use messages::{
     client::ClientMessages,
     server::{Bullet, Tank, Wall},
 };
+use rand::{random, random_bool, random_range};
 
 fn handle_map_change(walls: &[Wall]) -> Option<ClientMessages> {
     todo!(
@@ -10,8 +11,16 @@ fn handle_map_change(walls: &[Wall]) -> Option<ClientMessages> {
         walls
     )
 }
-fn handle_state_change(tanks: &[Tank], bullets: &[Bullet]) -> Option<ClientMessages> {
-    todo!("Handle state change:\n{:?}\n{:?}\n", tanks, bullets)
+fn handle_state_change(_tanks: &[Tank], _bullets: &[Bullet]) -> Option<ClientMessages> {
+    Some(ClientMessages::control(
+        [
+            if random_bool(0.6) { 1000.0 } else { -1000.0 },
+            if random_bool(0.6) { 1000.0 } else { -1000.0 },
+        ],
+        if random_bool(0.5) { 10.0 } else { -10.0 },
+        false,
+        // random_bool(0.6),
+    ))
 }
 
 fn main() {
